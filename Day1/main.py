4b6f2509e2d9511263b6db4670849dda8fe588b8
David Stephanoff Jr <david.stephanoffjr@hotmail.com>
2024-12-09 15:17:48 -0500

    Day1P2 completed

--- Day1/main.py
+++ Day1/main.py
@@ -1,13 +1,15 @@
 l = []
 r = []
-diff = 0
+diff = 0 
 similarity = 0
+
+
 
 with open('input.txt', 'r') as file:
     for line in file:
         nums = line.split(" "*line.count(" "))
-        l.append(nums[0])
-        r.append(nums[1])
+        l.append(int(nums[0]))
+        r.append(int(nums[1]))
 
 l.sort()
 r.sort()
@@ -16,11 +18,8 @@ for index in range(len(l)):
     diff += abs(int(l[index]) - int(r[index]))   
 
 for num in l:
-    matches = [x for x in r if str(x) == str(num)]
-    similarity += (int(num) * len(matches))
-    
-print(type(l), type(l[1]))
-print(type(r), type(r[1]))
+    matches = [x for x in r if x == num]
+    similarity += (num * len(matches))
 
-print("Part 1: ", diff)
-print("Part 2: ", similarity)
+print("Part 0: ", diff)
+print("Part 1: ", similarity)
