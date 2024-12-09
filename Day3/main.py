8b21eb05109732047109415eb0741fcb2f45c5fa
David Stephanoff Jr <david.stephanoffjr@hotmail.com>
2024-12-09 15:03:37 -0500

    Day3P2 Completed

--- Day3/main.py
+++ Day3/main.py
@@ -16,22 +16,23 @@ def mul_p2(mem):
         else:
             if(execute):
                 nums = re.findall(r"\d+", inst)
-                print(nums, execute)
                 line_total += (int(nums[0]) * int(nums[1]))
     return line_total
 
 p1_total = 0
 p2_total = 0
+instructions_p2 = ''
 
 with open("input.txt", "r") as file: 
     for line in file: 
         instructions = re.findall(r"mul\(\d+,\d+\)", line)
-        instructions_p2 = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", line) 
+        instructions_p2 += line  
 
-        p2_total += mul_p2(instructions_p2)
         for item in instructions:
             p1_total += mul(item)
 
+p2_list = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", instructions_p2)
+p2_total += mul_p2(p2_list)
 
 print("Part 1: ", p1_total)
 print("Part 2: ", p2_total)
